2610e86f3a990e69b849d6ac8930e6657b7f0a15
sonaksh <sonaksh@swiftace.ai>
2019-07-22 22:28:30 +0530

    Updates

---
+++ precache-manifest.5309f5ebdd2955c1e6196bd7f09b613a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0305ee230ec2c76abd3f472c705cefc",
+    "url": "/tour-and-travel-app/index.html"
+  },
+  {
+    "revision": "551850d897800072a202",
+    "url": "/tour-and-travel-app/static/css/main.c5bda5a7.chunk.css"
+  },
+  {
+    "revision": "4762d17a40e51fba3eec",
+    "url": "/tour-and-travel-app/static/js/2.60e46ddf.chunk.js"
+  },
+  {
+    "revision": "551850d897800072a202",
+    "url": "/tour-and-travel-app/static/js/main.78e2da0d.chunk.js"
+  },
+  {
+    "revision": "3b209514e111ace87aa9",
+    "url": "/tour-and-travel-app/static/js/runtime~main.80d85ef7.js"
+  }
+]);
